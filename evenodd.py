e468fc9babfdbcd26d517320545130f46a96138e
kamble0805 <sekamblehome@gmail.com>
2025-05-21 10:24:43 +0530

    assignment 2

---
+++ evenodd.py
@@ -0,0 +1,7 @@
+def EvenOdd():
+    num = float(input("enter number"))
+    if num % 2 == 0:
+        print("even")
+    else:
+        print("odd")
+EvenOdd()
---
+++ loop.py
@@ -0,0 +1,7 @@
+def loop():
+    sum = 0
+    for number in range(1, 50):
+        sum = sum + number
+    print(sum)
+
+loop()
